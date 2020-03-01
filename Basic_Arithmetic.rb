f66845d405f52034e2192c4b70657fc263bede34
Francisco-Castro <zipcodeFrancisco@gmail.com>
2020-02-29 21:46:38 -0500

    Added a file for using Basic Arithmetics

---
+++ Basic_Arithmetic.rb
@@ -0,0 +1,24 @@
+
+p 2 + 3
+p 2 - 3
+p 2 * 3
+p 2 / 3
+# Concatenation
+p "School" + "bus"
+# Decimal
+p 12 / 5
+p 12 / 5.0
+p 12.0 / 5
+p 12.0 / 5.0
+# Power
+p "Exponent"
+p 2 ** 4
+# Module
+p "Module"
+p 0 % 3
+p 1 % 3
+p 2 % 3
+p 3 % 3
+p 4 % 3
+p 5 % 3
+p 6 % 3
